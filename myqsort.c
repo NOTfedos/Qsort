1bae589aafac2b737760b329e636872f33b0ea50
NOTfedos <mistkap@gmail.com>
2020-11-06 13:25:05 +0300

    'refactor'

---
+++ myqsort.c
@@ -0,0 +1,34 @@
+#include "myqsort.h"
+
+
+void swap(double *x, double *y){
+	double tmp = *x;
+	*x = *y;
+	*y = tmp;
+}
+
+
+int partition(double *arr, int start, int end){
+	int marker = start;
+	for(int i = start; i < end; i++){
+		if (arr[i] < arr[end]){ // берём последний элемент за опорный
+			swap(&arr[i], &arr[marker]); // убираем в левую часть все элементы, меньшие опорного
+			marker++; // увеличиваем вместимость левой части
+		}
+	}
+	swap(&arr[marker], &arr[end]); // меняем опорный элемент на разделитель
+	return marker;
+}
+
+
+void myqsort(double *arr, int start, int end){
+	// функция qsort
+	
+	if (start >= end){
+		return;
+	}
+
+	int pivot = partition(arr, start, end); // находим опорный элемент
+	myqsort(arr, start, pivot-1); // сортируем левую часть
+	myqsort(arr, pivot+1, end); // сортируем правую часть
+}
---
+++ visual.c
@@ -0,0 +1,165 @@
+#include "myqsort.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "stdbool.h"
+#include "string.h"
+
+
+void output_array(double *arr, int n);
+
+
+void write_array_to_file(const char *outputFileName, double *array, int n);
+
+
+double * read_array_from_file(const char *inputFileName, int *n);
+
+
+int main(int argc, char **argv){
+
+	bool isRead = 0, isWrite = 0;
+	char *inputFileName, *outputFileName;
+
+
+	// обрабатываем аргументы командной строки
+	for(int i=1; i < argc; i++){ 
+
+		if (!strcmp(argv[i], "-input") && !isRead){
+			if (i < (argc - 1)){
+				inputFileName = argv[i+1];
+				isRead = 1;
+			}
+		}
+
+
+		if (!strcmp(argv[i], "-output") && !isWrite){
+			if (i < (argc - 1)){
+				outputFileName = argv[i+1];
+				isWrite = 1;
+			}
+		}
+
+	}
+
+
+	// данные нашего исходного массива(нуждающегося в сортировке)
+	int n; double *array;
+
+
+	// пытаемся прочитать массив из файла
+	if (isRead){
+		array = read_array_from_file(inputFileName, &n);
+		if (array == NULL){
+			free(array);
+			printf("Some error while reading array from file\n");
+			return 0;
+		}
+		output_array(array, n);
+	}else{ 
+		printf("No input files\n");
+		free(array);
+		return 0;
+	}
+
+
+	// сортируем массив
+	myqsort(array, 0, n-1);
+
+
+	// вывод в файл отсортированного массива
+	if(isWrite){
+		write_array_to_file(outputFileName, array, n);
+		printf("\nSorted Array written into %s\n", outputFileName);
+	}else{
+		printf("\nSorted Array is NOT written\n");
+	}
+
+
+	//освобождаем память
+	free(array); 
+
+
+	return 0;
+}
+
+
+void output_array(double *arr, int n){
+	// вывод одномерного массива(DEBUG ONLY)
+	printf("-------------------------------------\n");
+	printf("%s\n", "ARRAY IS:");
+	for(int i=0; i<n; i++){
+		printf("%lf | ", arr[i]);
+	}
+	printf("\n");
+	printf("-------------------------------------\n");
+}
+
+
+void write_array_to_file(const char *outputFileName, double *array, int n){
+	// выводим массив(отсортированный) в файл
+
+
+	FILE *outputFile = fopen(outputFileName, "w");
+
+
+	printf("\nTrying to write into file %s\n", outputFileName);
+
+	fflush(outputFile);
+	fprintf(outputFile, "%d ", n);
+
+
+	for(int i = 0; i < n; i++){
+		fprintf(outputFile, "%lf ", array[i]);
+	}
+	fprintf(outputFile, "%s\n", "");
+	
+
+	fclose(outputFile);
+	printf("Success\n");
+
+}
+
+
+double * read_array_from_file(const char *inputFileName, int *n){
+	// считываем одномерный массив из файла
+	
+
+	FILE *datafile;
+
+
+	printf("\nTrying to read file %s\n", inputFileName);
+
+
+	// открываем файл
+	datafile = fopen(inputFileName, "r"); 
+	if (datafile == NULL){ // если файл не существует
+		printf("Can not open file %s\n", inputFileName);
+		fclose(datafile);
+		return NULL;
+	}
+
+
+	printf("Success\n");
+
+
+	// сбрасываем буфер для файла
+	fflush(datafile);
+	// считываем кол-во элементов массива
+	fscanf(datafile, "%d ", n);
+
+
+	// выделяем память под массив
+	double *array = (double *) malloc((*n) * sizeof(double));
+	for(int i=0; i < (*n); i++){
+		fscanf(datafile, "%lf ", &array[i]);
+	}
+
+
+	fclose(datafile);
+
+
+	// DEBUG output array
+	// output_array(array, *n);
+
+
+	return array;
+}
